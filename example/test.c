ce78890afd654fdb849d62f0ef6296108a216cf6
Azuremuzzlekit <azuremuzzlekit@gmail.com>
2025-03-30 13:35:38 +0300

    first commit
    release 1.0.0

---
+++ example/test.c
@@ -0,0 +1,17 @@
+// a bit of js style :)
+
+#include "../include/libbenchmark.h"
+#include <unistd.h>
+
+int main() {
+    // test of empty cycle
+    BENCH(1000, {
+        // empty block
+    });
+
+    BENCH(10000000, {
+        write(STDOUT_FILENO, NULL, 0);
+    });
+
+    return 0;
+}
