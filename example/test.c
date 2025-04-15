4c983df838c08723c5af68fc2d83dc45356fdd3a
Azuremuzzlekit <azuremuzzlekit@gmail.com>
2025-04-15 20:04:42 +0300

    Update test.c

--- example/test.c
+++ example/test.c
@@ -1,16 +1,18 @@
+// example of using the bench.h library
+
 #include "bench.h"
 
 void example() {
-    volatile int x = 0;
+    volatile int x = 0; // volatile prevents optimization
     
     BENCH("Empty loop", {
         for(int i = 0; i < 1000; i++) {
-            x = i;
+            x = i; // pseudo-load
         }
     }, 1000);
 
     BENCH_RDTSC("CPU Cycles test", {
-        asm("nop");
+        asm("nop"); // precise measurement of one instruction
     }, 100000);
 }
 
